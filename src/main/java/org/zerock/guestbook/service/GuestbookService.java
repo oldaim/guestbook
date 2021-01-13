ba7f3d124c150addc8921be64fc1dc63924c65c9
oldaim <dgk0911@gmail.com>
2021-01-13 14:02:23 +0900

    1/13-DTO ,service ,jpa 관련코드

--- src/main/java/org/zerock/guestbook/service/GuestbookService.java
+++ src/main/java/org/zerock/guestbook/service/GuestbookService.java
@@ -1,12 +1,16 @@
 package org.zerock.guestbook.service;
 
 import org.zerock.guestbook.dto.GuestbookDTO;
+import org.zerock.guestbook.dto.PageRequestDTO;
+import org.zerock.guestbook.dto.PageResultDTO;
 import org.zerock.guestbook.entity.Guestbook;
 
 public interface GuestbookService {
     Long register(GuestbookDTO dto);
 
-    default Guestbook dtoTOEntity(GuestbookDTO dto){
+    PageResultDTO<GuestbookDTO,Guestbook> getList(PageRequestDTO requestDTO);
+
+    default Guestbook dtoTOEntity(GuestbookDTO dto){ //dto->entity 교환
         Guestbook entity = Guestbook.builder()
                 .gno(dto.getGno())
                 .title(dto.getTitle())
@@ -14,5 +18,17 @@ public interface GuestbookService {
                 .writer(dto.getWriter())
                 .build();
         return entity;
+    }
+
+    default GuestbookDTO entityTODto(Guestbook entity){//entity->dto 교환
+        GuestbookDTO dto = GuestbookDTO.builder()
+                .gno(entity.getGno())
+                .title(entity.getTitle())
+                .content(entity.getContent())
+                .writer(entity.getWriter())
+                .regTime(entity.getRegDate())
+                .modDate(entity.getModDate())
+                .build();
+        return dto;
     }
 }
--- src/test/java/org/zerock/guestbook/service/GuestbookServiceTests.java
+++ src/test/java/org/zerock/guestbook/service/GuestbookServiceTests.java
@@ -1,9 +1,12 @@
 package org.zerock.guestbook.service;
 
+import ch.qos.logback.classic.net.SimpleSocketServer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.zerock.guestbook.dto.GuestbookDTO;
+import org.zerock.guestbook.dto.PageRequestDTO;
+import org.zerock.guestbook.dto.PageResultDTO;
 import org.zerock.guestbook.entity.Guestbook;
 
 @SpringBootTest
@@ -21,6 +24,26 @@ public class GuestbookServiceTests {
                 .build();
         System.out.println(service.register(guestbookDTO));
 
+    }
+
+    @Test
+    public void testList(){
+
+        PageRequestDTO pageRequestDTO =PageRequestDTO.builder().page(1).size(10).build();
+
+        PageResultDTO<GuestbookDTO,Guestbook> resultDTO =service.getList(pageRequestDTO);
+
+        System.out.println("PREV: "+resultDTO.isPrev());
+        System.out.println("NEXT: "+resultDTO.isNext());
+        System.out.println("TOTAL "+resultDTO.getTotalPage());
+
+        System.out.println("---------------------------------------------------");
+        for(GuestbookDTO guestbookDTO :resultDTO.getDtoList()){
+            System.out.println(guestbookDTO);
+        }
+
+        System.out.println("===================================================");
+        resultDTO.getPageList().forEach(i-> System.out.println(i));
     }
 
 }
